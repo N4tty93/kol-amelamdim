33cede9de5208baa650834dccf4a2bce0c17cb9a
Netanel Vaknin <netanel.vaknin@zeekit.me>
2022-08-06 20:54:11 +0300

    remove baseUrl

--- apps/kol-amelamdim/pages/_app.tsx
+++ apps/kol-amelamdim/pages/_app.tsx
@@ -6,7 +6,7 @@ import rtlPlugin from 'stylis-plugin-rtl';
 import { CacheProvider } from '@emotion/react';
 import createCache from '@emotion/cache';
 import { prefixer } from 'stylis';
-import theme from 'theme';
+import theme from '../theme';
 import './styles.css';
 
 const cacheRtl = createCache({
