1166df7aacfa4dd99d0872e79e7bb7f35df01874
Netanel Vaknin <netanel.vaknin@zeekit.me>
2022-08-20 20:38:45 +0300

    Fix build

--- libs/models/src/lib/user.ts
+++ libs/models/src/lib/user.ts
@@ -26,5 +26,5 @@ const UserSchema = new mongoose.Schema(
   { collection: 'my-users' }
 );
 
-export const User: mongoose.Model<IUser & mongoose.Document> =
+export const User =
   mongoose.models['UserSchema'] || mongoose.model('UserSchema', UserSchema);
