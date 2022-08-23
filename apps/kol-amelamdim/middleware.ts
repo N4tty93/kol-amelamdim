383debc1ebe97079a4661353f32defb9f043472d
david <david.doskalovich@zeekit.me>
2022-08-23 20:06:24 +0300

    block getting inside login/register when user is authenticated

--- apps/kol-amelamdim/middleware.ts
+++ apps/kol-amelamdim/middleware.ts
@@ -3,16 +3,14 @@ import type { NextRequest } from 'next/server';
 import { verify } from './services/jwt_sign_verify';
 
 export async function middleware(request: NextRequest) {
-  const jwt = request.cookies.get('adminToken');
-
   const { pathname } = request.nextUrl;
-
+  const userToken = request.cookies.get('token');
   if (request.nextUrl.pathname.startsWith('/admin/')) {
+    const jwt = request.cookies.get('adminToken');
     if (jwt === undefined) {
       request.nextUrl.pathname = '/login';
       return NextResponse.redirect(request.nextUrl);
     }
-
     try {
       verify(jwt, process.env.ADMIN_TOKEN_SECRET);
       return NextResponse.next();
@@ -22,12 +20,22 @@ export async function middleware(request: NextRequest) {
     }
   } else if (pathname.startsWith('/api/upload-file')) {
     try {
-      const token = request.cookies.get('token');
-      await verify(token, process.env.ACCESS_TOKEN_SECRET);
+      await verify(userToken, process.env.ACCESS_TOKEN_SECRET);
       return NextResponse.next();
     } catch (error) {
       request.nextUrl.pathname = '/login';
       return NextResponse.redirect(request.nextUrl);
+    }
+  } else if (
+    request.nextUrl.pathname.startsWith('/login') ||
+    request.nextUrl.pathname.startsWith('/register')
+  ) {
+    try {
+      await verify(userToken, process.env.ACCESS_TOKEN_SECRET);
+      request.nextUrl.pathname = '/';
+      return NextResponse.redirect(request.nextUrl);
+    } catch (error) {
+      return NextResponse.next();
     }
   } else {
     return NextResponse.next();
