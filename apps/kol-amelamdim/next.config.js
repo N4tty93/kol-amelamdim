7fea792c4cabaa2a9fdc2b36455ddf03ce0d3032
Netanel Vaknin <netanel.vaknin@zeekit.me>
2022-10-07 21:46:33 +0300

    Fix env

--- apps/kol-amelamdim/next.config.js
+++ apps/kol-amelamdim/next.config.js
@@ -11,16 +11,6 @@ const nextConfig = {
     // See: https://github.com/gregberge/svgr
     svgr: false,
   },
-  env: {
-    S3_ACCESS_KEY: 'AKIAZSW6X7C4T4DX2MUW',
-    S3_SECRET_KEY: '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',
-    AWS_BUCKET_NAME: 'kol-amelamdim-bucket',
-    MONGO_URI:
-      'mongodb+srv://david:771992@cluster0.0tyx9p1.mongodb.net/?retryWrites=true&w=majority',
-    ACCESS_TOKEN_SECRET:
-      '129d4cd978d5fedb613bba2140205d9bb6870c45e41a0b562cda47ce31981f50cc89eafba452b0b56ef6ca9f4afb3036ff575a662dc01418e950674334781568',
-    ADMIN_TOKEN_SECRET: 'kolamelamdim2022!',
-  },
   i18n,
 };
 
