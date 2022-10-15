8a1b737e1397bc587dfc4a4457811b1022af954e
Netanel <netanel.vaknin@zeekit.me>
2022-10-15 16:33:41 +0300

    Test

--- apps/kol-amelamdim/pages/index.tsx
+++ apps/kol-amelamdim/pages/index.tsx
@@ -23,6 +23,7 @@ import { GetStaticPropsContext } from 'next';
 import i18nConfig from '../next-i18next.config';
 import { useTranslation } from 'next-i18next';
 import connect from '../db/connectMongo';
+import { WeeklyArticle } from '@kol-amelamdim/models';
 
 const CategoryCard = styled(Card)`
   height: 90px;
@@ -234,11 +235,14 @@ export function Home({ activeArticle }) {
 export async function getStaticProps({ locale }: GetStaticPropsContext) {
   try {
     await connect();
-    const activeArticle = await axios.get('/api/get-active-weekly-article');
+    const activeArticle = await WeeklyArticle.findOne({
+      isActiveArticle: true,
+    });
+
     return {
       props: {
         ...(await serverSideTranslations(locale, ['home'], i18nConfig)),
-        activeArticle: activeArticle.data,
+        activeArticle: JSON.parse(JSON.stringify(activeArticle)),
       },
     };
   } catch (e) {
