e44b0f5cb1d1aae48737dbf208113ae0bd086fe3
david <david.doskalovich@zeekit.me>
2022-10-15 11:33:56 +0300

    check categories db

--- apps/kol-amelamdim/pages/category/[category].tsx
+++ apps/kol-amelamdim/pages/category/[category].tsx
@@ -28,13 +28,13 @@ import i18nConfig from '../../next-i18next.config';
 
 const rowsPerPage = 25;
 
-const CategoryPage = ({ files, error }) => {
+const CategoryPage = ({ files, error, contextTest }) => {
   const [fileType, setFileType] = useState('');
   const [filterText, setFilterText] = useState('');
   const [page, setPage] = useState<number>(0);
   const [filteredFiles, setFilteredFiles] = useState<IFile[]>([]);
   const [isUploadFileDialogOpen, setIsUploadFileDialogOpen] = useState(false);
-
+  console.log('contextTest', contextTest);
   const router = useRouter();
   const { t } = useTranslation('category');
   const { category } = router.query;
@@ -175,6 +175,7 @@ export async function getStaticProps(context) {
       props: {
         files: data.files,
         error: false,
+        contextTest: context,
         ...(await serverSideTranslations(
           context.locale,
           ['category', 'home'],
