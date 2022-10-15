47747870b31a5401ebbb9458298d20c0378e7ec7
david <david.doskalovich@zeekit.me>
2022-10-15 17:16:44 +0300

    check categories db

--- apps/kol-amelamdim/api/index.ts
+++ apps/kol-amelamdim/api/index.ts
@@ -2,7 +2,9 @@ import axios from 'axios';
 
 const axiosConfig = {
   baseURL:
-    process.env.NODE_ENV === 'development' ? 'http://localhost:4200' : '/',
+    process.env.NODE_ENV === 'development'
+      ? 'http://localhost:4200'
+      : process.env.VERCEL_URL,
   headers: {
     'Content-type': 'application/json',
     Accept: 'application/json',
